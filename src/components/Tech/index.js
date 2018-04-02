2adb0116d3d2db00b06ab1396dfbeaa810b889fb
Laura-O <laura.ohrndorf@gmail.com>
2018-04-02 22:41:16 +0200

    About section

--- src/components/Tech/index.js
+++ src/components/Tech/index.js
@@ -4,9 +4,44 @@ import Link from 'gatsby-link';
 import styles from './styles.module.scss';
 
 const Tech = ({ stack }) => {
-  const frontEnd = [];
+  const frontEnd = ['React', 'Redux', 'Gatsby', 'Vue.js', 'HTML5', 'SCSS'];
+  const backEnd = ['Node', 'Express', 'PostgreSQL'];
+  const other = ['Git', 'R'];
 
-  return <div className="tech" />;
+  return (
+    <div className={styles.tech}>
+      <div className={styles.techbox}>
+        <h1>Frontend</h1>
+        <ul className={styles.tags}>
+          {frontEnd.map(tech => (
+            <li key={tech}>
+              <a href="#">{tech}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className={styles.techbox}>
+        <h1>Backend</h1>
+        <ul className={styles.tags}>
+          {backEnd.map(tech => (
+            <li key={tech}>
+              <a href="#">{tech}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className={styles.techbox}>
+        <h1>Other</h1>
+        <ul className={styles.tags}>
+          {other.map(tech => (
+            <li key={tech}>
+              <a href="#">{tech}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Tech;
--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,28 @@ import Tech from '../components/Tech';
 const AboutPage = () => (
   <div className="about">
     <div className="about-upper">
-      <div className="about-upper-left">left</div>
+      <div className="about-upper-left">
+        <div>I am a full-stack developer based in Germany.</div>
+        <div>
+          I started developing when I was 13 and started my very first website
+          on
+          <a href="https://en.wikipedia.org/wiki/Yahoo!_GeoCities">
+            Geocities
+          </a>.
+        </div>
+        <div>
+          My excitement for web technologies hasn't changed since then.
+          Meanwhile, I have a degree in computer science and also a Ph.D. in
+          computer science education.
+        </div>
+        <div>
+          I have experience with several JavaScript frameworks, but I mainly
+          focus on React today. React has a simple but awesome API, which
+          provides a lot of flexibility. This website was built with{' '}
+          <a href="https://www.gatsbyjs.org/">Gatsby.js</a>, which is a static
+          site generator for React.
+        </div>
+      </div>
       <div className="about-upper-right">
         <Tech />
       </div>
