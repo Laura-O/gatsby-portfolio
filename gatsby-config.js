0d8e7c694d0ebde935d593036c4ac5cd098406d2
Laura-O <laura.ohrndorf@gmail.com>
2018-04-12 15:56:42 +0200

    Fix responsiveness of prismjs code blocks

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,8 +39,8 @@ module.exports = {
           {
             resolve: 'gatsby-remark-images',
             options: {
-              linkImagesToOriginal: false,
-              maxWidth: 590,
+              maxWidth: 750,
+              backgroundColor: `#ffffff`,
             },
           },
         ],
