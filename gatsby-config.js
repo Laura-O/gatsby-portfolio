5045a2579786e3797ab8afdf69ec3a11fd0f2f7d
Laura-O <laura.ohrndorf@gmail.com>
2018-04-12 13:31:45 +0200

    Make it a PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-offline`,
     {
       resolve: 'gatsby-source-filesystem',
       options: {
