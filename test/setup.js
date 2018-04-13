d9c2dbd2189d8d52ccfc67c075e1da35fadbf121
Laura-O <laura.ohrndorf@gmail.com>
2018-04-13 13:20:53 +0200

    Add jest and enzyme, change file names

---
+++ test/setup.js
@@ -0,0 +1,7 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+// Setup the global graphql tagged template literals
+global.graphql = args => args;
