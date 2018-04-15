ff354603d779f0adff755ae6f7498480b85591b9
Laura-O <laura.ohrndorf@gmail.com>
2018-04-16 00:42:38 +0200

    Add code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{js,jsx}', '!src/components/icons/*'],
+  coverageDirectory: './coverage/',
   coveragePathIgnorePatterns: ['Contact.js', 'Fireworks.js'],
   moduleNameMapper: {
     '\\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
