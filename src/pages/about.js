d533714b25523f9f06ca2d8df5ecaee9d4e2f751
Laura-O <laura.ohrndorf@gmail.com>
2018-04-15 17:16:39 +0200

    Fix weird css error part 999

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,21 @@ import Link from 'gatsby-link';
 import Tech from '../components/Tech/Tech';
 import Bio from '../components/Bio/Bio';
 import Footer from '../components/Footer/Footer';
+import styled from 'styled-components';
+import * as constants from '../layouts/shared/style-constants';
+
+const AboutWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  background-color: white;
+  width: 100%;
+  color: $textColor;
+  align-items: center;
+`;
 
 const AboutPage = () => (
-  <div className="about">
+  <AboutWrapper>
     <div className="about-upper">
       <div className="about-upper-left">
         <Bio />
@@ -19,7 +31,7 @@ const AboutPage = () => (
       <Link to="/contact">Contact form</Link>.
     </div>
     <Footer />
-  </div>
+  </AboutWrapper>
 );
 
 export default AboutPage;
