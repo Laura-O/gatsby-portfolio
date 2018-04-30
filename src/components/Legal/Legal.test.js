2c372d96d8de82983e4f2729b5800307fb37cdf8
Laura-O <laura.ohrndorf@gmail.com>
2018-04-30 14:23:01 +0200

    Remove duplicate

--- src/components/Legal/Legal.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Legal from './Legal';
-
-test('rendering', () => {
-  const wrapper = shallow(<Legal />);
-  expect(wrapper).toMatchSnapshot();
-});
